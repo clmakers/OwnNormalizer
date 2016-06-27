d59d09d91cc07151ae270b3859ee99864863ace3
Domingo Sambo <dsambo@Domingos-MacBook-Pro.local>
2016-06-26 22:54:17 -0400

    First Alpha Puplished

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-var express = require("express"),  
-    app = express(),
-    bodyParser  = require("body-parser");
+/*!
+ * 
+ * OwnNormalizer
+ * MIT Licensed
+ * 
+ */
 
-var ownNormalizer = require('./lib/ownnormalizer')({
-    modulesPath: "/build/modules",
-    dateLang: "es"
-});
+'use strict';
 
-var data = {
-    id: "FDKFJSKFJDSKFSDJKSDFK",
-    title: "Titulos <p> <script>alert('algo');</script>",
-    description: "Descripcion del titulo de prueba",
-    date: "06/26/2016", // MM-DD-YYYY || MM DD YYYY || MM-DD-YYYY || long
-    votes:10,
-    isTrending:true,
-    email:"sample@domain.com",
-    phone:'+90.2322456789'
-};
-
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
-
-var router = express.Router();
-
-router.get('/', function(req, res) {  
-   
-   ownNormalizer.validate(data, "test", "onNewTestCreeated", function (objFinal) {
-    console.log(objFinal);
-});
-   
-   res.send("Enviado");
-});
-
-app.use(router);
-
-app.listen(3000, function() {  
-  console.log("Node server running on http://localhost:3000");
-});
+module.exports = require('./lib/ownnormalizer');
